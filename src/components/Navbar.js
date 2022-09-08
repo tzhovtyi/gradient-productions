b968e8e794f7c3dc88acfc75a2c6976c1d099e8e
tzhovtyi <tzhovtyy@mail.ru>
2022-09-08 12:58:42 +1000

    Add responsive navbar; update SM icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/navbar.css";
 import {useNavigate} from "react-router-dom";
 
 
 export default function Navbar() {
     const navigate = useNavigate();
+    const [menu, setMenu] = useState(false);
+
     return (
         <div className='nav-main'>
-            <div className = 'brand-name' onClick={() => navigate("/")}>Gradient Productions</div>
-            <div className='nav-right'>
+            <div className='mobile-bar'>
+                <div className = 'brand-name' onClick={() => navigate("/")}>Gradient Productions</div>
+                <a onClick={()=> setMenu(prevState => !prevState)} href='#' className='toggle-button'>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                </a>
+            </div>
+
+            <div className = {`nav-right ${menu &&'menu-active'}`}>
                 <div onClick={() => navigate("/portfolio")}>Portifolio</div>
                 <div onClick={() => navigate("/contacts")}>Contacts</div>
                 <div onClick={() => navigate("/rent")}>Shop</div>
             </div>
         </div>
-
     )
 }
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -10,9 +10,9 @@ export default function Contacts() {
             <p>Email: aaa@aa <br/>Phone: +1(222)3333333 <br/>Address: Cedar City, UT, 84720</p>
             <h3>Social Media</h3>
             <div className='icons-container'>
-                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="fa fa-youtube"></a>
-                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="fa fa-instagram"></a>
-                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="fa fa-facebook"></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="icon youtube"></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="icon instagram"></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=1-Rs6Wz-Rdw&t=64s&ab_channel=%D0%A8%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80%D0%92%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9%D0%95%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%B5%D0%BD%D0%BA%D0%BE" class="icon facebook"></a>
             </div>
         </div>
     )
